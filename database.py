0ef30f4fbe8ee66a61ca17beea50ac083338eda0
tomasz.molenda <tomasz.molenda@gmail.com>
2019-02-02 01:21:50 +0100

    database as singleton

--- database.py
+++ database.py
@@ -1,6 +1,16 @@
 class Database:
+    __instance = None
+    @staticmethod
+    def getInstance():
+        if Database.__instance == None:
+            Database()
+        return Database.__instance
 
     def __init__(self):
+        if Database.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Database.__instance = self
         self.test = []
 
     def insert(self, _id):
